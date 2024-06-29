342584251a0d29bd32690870842ef72936a26ed6
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-29 12:53:05 -0600

    34 - Rendering Django Form Validation Errors in Nextjs

--- src/waitlists/api.py
+++ src/waitlists/api.py
@@ -28,7 +28,7 @@ def list_wailist_entries(request):
 # /api/waitlists/
 @router.post("", 
     response={
-        200: WaitlistEntryDetailSchema,
+        201: WaitlistEntryDetailSchema,
         400: ErrorWaitlistEntryCreateSchema
     },
     auth=helpers.api_auth_user_or_annon
@@ -45,7 +45,7 @@ def create_waitlist_entry(request, data:WaitlistEntryCreateSchema):
     if request.user.is_authenticated:
         obj.user = request.user
     obj.save()
-    return obj
+    return 201, obj
 
 
 @router.get("{entry_id}/", response=WaitlistEntryDetailSchema, auth=helpers.api_auth_user_required)
